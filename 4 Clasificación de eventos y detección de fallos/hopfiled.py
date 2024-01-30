b722dce0c010735ee2f73793bb06a250bd98c5b1
Fran <franfi17sw@gmail.com>
2024-01-30 19:26:51 +0100

    Añadido modelo de hopfield

--- 4 Clasificación de eventos y detección de fallos/hopfiled.py
+++ 4 Clasificación de eventos y detección de fallos/hopfiled.py
@@ -12,6 +12,13 @@ class HopfieldNet:
         Entrena la red de Hopfield con los patrones de entrenamiento
         Siguiendo la regla de actualización: W = (sum(p_i * p_i^T) - I)/N
         patterns: lista de patrones de entrenamiento
+
+        Como sabemos, los pesos de la red de Hopfield nos indican la correlación
+        entre las neuronas. Esto es que si dos neuronas (pixeles en la imagen en nuestro caso)
+        están correlacionadas, cuando una de ellas cambie de estado, la otra también lo hará.
+        Es decir que si un pixel vale 1, el otro también valdrá 1 y si uno vale -1, el otro también.
+
+        Dando la matriz de pesos una matriz de 1 y -1 de N x N, donde N es el número de neuronas, en nuestro caso el número de pixeles de la imagen. 
         
         '''
         for pattern in patterns:
@@ -25,7 +32,7 @@ class HopfieldNet:
         # Esta condición es como restar la matriz identidad
         np.fill_diagonal(self.weights, 0)
         # Normalizamos los pesos dividiendo por el tamaño (dividir por N)
-        self.weights /= self.size
+        self.weights /= len(patterns)
     
     def update(self, state: np.ndarray, steps: int=1, activation_function=np.sign)->np.ndarray:
         '''
@@ -33,6 +40,10 @@ class HopfieldNet:
         state: estado inicial de la red
         steps: número de pasos de actualización
         activation_function: función de activación
+
+
+        Como sabemos, la red de Hopfield es como un algoritmo de metropolis Hasting con temperatura 0 en este caso, es decir que solo acepta estados que disminuyan la energía de la red. 
+        Así la dinámica consiste en elegir una neurona al azar y actualizarla, invirtiendo su valor si esto disminuye la energía de la red, que está contenida en la matriz de pesos.
         '''
         state = state.flatten() # Aplanamos el estado inicial
         for _ in range(steps):
@@ -52,6 +63,8 @@ class HopfieldNet:
         Siendo b el bias (en nuestro caso no hay bias)
 
         state: estado de la red
+
+        Aquí vemos como el parámetro que usamos para actualizar la red en la función anterior, es la energía de la red sin bias.
         '''
         state = state.flatten()
         # Calculamos la energía de la red
@@ -66,11 +79,12 @@ class HopfieldNet:
         state = state.reshape((self.image_shape[0], self.image_shape[1]))
         return state
 
-def preprocesar(imagen, umbral=128)->np.ndarray:
+def preprocesar(imagen, umbral=128, shape=(75,75))->np.ndarray:
     imagen = Image.open(imagen)
-    imagen = imagen.resize((75, 75))
+    imagen = imagen.resize(shape)
     imagen = imagen.convert('L')
     imagen = imagen.point(lambda x: -1 if x < umbral else 1)
     imagen = np.array(imagen)
     imagen = np.where(imagen == 0, -1, imagen)
     return imagen
+
